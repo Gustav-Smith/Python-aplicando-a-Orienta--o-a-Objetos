d5f073e72f7c115260ec3ad874e8dcfd6c4bede7
Gustav-Smith <gustavojeanpt@gmail.com>
2024-05-21 23:07:34 -0300

    Python POO

--- exercicios/Restaurante.py
+++ exercicios/Restaurante.py
@@ -1,7 +1,11 @@
 class Restaurante:
-    def __init__(self, categoria, ativo=False, capacidade, nota_avaliacao):
+    def __init__(self, nome, categoria, ativo=False, capacidade, nota_avaliacao):
         self.nome = nome
         self.categoria = categoria
         self.ativo = ativo
         self.capacidade = capacidade
         self.nota_avaliacao = nota_avaliacao
+
+restaurante_exemplo = Restaurante(nome='Comida Boa', categoria='Gourmet', ativo=True, capacidade=50, nota_avaliacao=4.5)
+
+
